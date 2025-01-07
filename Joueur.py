758400545ef6821cbb2e89c34b1d9ac55542bc48
julien <julien.fabre05@etu.umontpellier.fr>
2025-01-07 10:24:18 +0100

    Création de la classe Joueur

--- Joueur.py
+++ Joueur.py
@@ -1,1 +1,8 @@
-Joueur
+class Joueur:
+    def __init__(self, x, y, hauteur, largeur):
+        self.x=x
+        self.y=y
+        self.hauteur=hauteur
+        self.largeur=largeur
+        pass
+    
