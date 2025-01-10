1bcc8a579cbc13be8bd9f2cf6973931dadf16f41
clement.frassier <clement.frassier@etu.umontpellier.fr>
2025-01-10 10:34:33 +0100

    debut creation du jeu

--- Ennemis.py
+++ Ennemis.py
@@ -1,8 +1,40 @@
-class Joueur:
-    def __init__(self, x, y, hauteur, largeur):
-        self.x=x
-        self.y=y
+class ennemi:
+    def __init__(self, position, hauteur, largeur,graphic,vitesse):
+        self.position = position
         self.hauteur=hauteur
         self.largeur=largeur
-        pass
-    
+        self.graphic=graphic
+        self.vitesse=vitesse
+
+    # Getter pour position
+    def get_position(self):
+        return self.position
+
+    # Setter pour position
+    def set_position(self, position):
+        self.position = position
+
+    # Getter pour graphic
+    def get_graphic(self):
+        return self.graphic
+
+    # Setter pour graphic
+    def set_graphic(self, graphic):
+        self.graphic = graphic
+
+    # Getter pour vitesse
+    def get_vitesse(self):
+        return self.vitesse
+
+    # Setter pour vitesse
+    def set_vitesse(self, vitesse):
+        self.vitesse = vitesse
+
+    """
+    Déplace l'ennemi selon dt et la vitesse
+    Paramètre :
+        dt : valeur du déplacement
+    """
+    def move_ennemi(self, dt):
+        self.position += dt * self.vitesse  
+
--- Projectile.py
+++ Projectile.py
@@ -1,8 +1,43 @@
-class Projectile:
-    def __init__(self,x,y):
-        self.X=x
-        self.y=y
-        self.degat=1
-
+class projectile:
+    def __init__(self,graphic,vitesse,degat,rayon,position):
+        self.position=position or [0,0]
+        self.degat=degat
+        self.graphic=graphic
+        self.vitesse=vitesse
+        self.rayon=rayon
         
     
+    # Setter pour position
+    def set_position(self, position):
+        self.position = position
+
+
+    # Getter pour degat
+    def get_degat(self):
+        return self.degat
+
+    # Setter pour degat
+    def set_degat(self, degat):
+        self.degat = degat
+
+    # Getter pour rayon
+    def get_rayon(self):
+        return self.rayon
+
+    # Getter pour vitesse
+    def get_vitesse(self):
+        return self.vitesse
+
+    # Setter pour vitesse
+    def set_vitesse(self, vitesse):
+        self.vitesse = vitesse
+
+    """
+    Déplace le projectile selon dt et la vitesse
+    Paramètre :
+        dt : valeur du déplacement
+    """
+    def move_projectile(self, dt):
+        self.position[0] += dt * self.vitesse[0]  # Déplacement horizontal
+        self.position[1] += dt * self.vitesse[1]  # Déplacement vertical  
+
