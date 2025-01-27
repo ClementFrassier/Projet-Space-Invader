56aba4c3d8107257b93aa07cb8a7d53be21e17a8
ClementFrassier <clement.frassier@gmail.com>
2025-01-27 23:40:28 +0100

    ajout anaconda

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -26,11 +26,10 @@ class spaceInvader :
         self.confirmed_text = ""  # Stocke le texte confirmé
         self.is_typing = False  # Flag pour détecter si on est en train de taper
         self.explosion_sound = explosion_sound
-<<<<<<< HEAD
+
         self.font = cv2.FONT_HERSHEY_SIMPLEX  
-=======
-        self.font = cv2.FONT_HERSHEY_SIMPLEX         
->>>>>>> de87c3c1c2bc7a683cb695bbe0ef96ca2a6cf62b
+
+     
 
 
     def mouse_callback(self, event, x, y, flags, param):
